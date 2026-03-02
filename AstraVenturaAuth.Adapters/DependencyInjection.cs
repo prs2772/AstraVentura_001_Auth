ab33381619297991ced20605b9f62db79506d4fa
Paris <117606280+prs2772@users.noreply.github.com>
2026-03-01 20:10:55 -0600

    AVA-001 .Adapters Add Dependency Inyection class and csproj updates

---
+++ AstraVenturaAuth.Adapters/DependencyInjection.cs
@@ -0,0 +1,36 @@
+// Adapters/DependencyInjection.cs
+using AstraVenturaAuth.Adapters.Drivens.Database;
+using AstraVenturaAuth.Adapters.Drivens.Security;
+using AstraVenturaAuth.Core.Ports.Drivens;
+using AstraVenturaAuth.Core.Ports.Drivers;
+using AstraVenturaAuth.Core.UseCases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AstraVenturaAuth.Adapters;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAdapters(this IServiceCollection services, IConfiguration config)
+    {
+        // DB, desde el puente de Factory o ya en docker desde Program.cs
+        services.AddDbContext<AuthDbContext>(opts =>
+            opts.UseSqlServer(config.GetConnectionString("AuthDb")));
+
+        // Inyecto mis implementaciones de los puertos (Drivens que se firman en Core, aqui los implemento)
+        services.AddScoped<IUserRepository, UserRepository>(); // Uno por request
+        services.AddScoped<ITokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IPasswordHasher, BCryptPasswordHasher>(); // No requiero más de una instancia, es stateless
+
+        // Inyecto mis casos de uso que se registran y definen también dentro de Core
+        services.AddScoped<IAuthenticateUserUseCase, AuthenticateUserUseCase>();
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        services.AddScoped<IRefreshTokenUseCase, RefreshTokenUseCase>();
+
+        // Configuración JWTm inyectando en mi clase JwtOption las opciones de configuración de appsettings.json
+        services.Configure<JwtOptions>(config.GetSection(JwtOptions.SectionName));
+
+        return services;
+    }
+}
